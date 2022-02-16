2e872d32929f93d59b963e6c1addb16a1c5088f5
TamerAli-0 <blackstar09770@gmail.com>
2022-02-17 02:12:42 +0400

    edited the info page

--- app.js
+++ app.js
@@ -54,3 +54,37 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
+
+
+const parentContainer =  document.querySelector('.read-more-container');
+
+parentContainer.addEventListener('click', event=>{
+
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
+
+
+// // const readMoreBtn = document.querySelector(".read-more-btn");
+// const readMoreBtn = document.querySelector(".read-more-btn");
+// const text = document.querySelector(".text");
+
+// readMoreBtn.addEventListener("click", (e) => {
+//   text.classList.toggle("show-more");
+//   if (readMoreBtn.innerText === "Read More") {
+//     readMoreBtn.innerText = "Read Less";
+//   } else {
+//     readMoreBtn.innerText = "Read More";
+//   }
+// });
+
